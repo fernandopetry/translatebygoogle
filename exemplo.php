b6e2467c59352d99a46391a24aaf4a0b9decfc4b
Fernando Petry <fernandopetry7@gmail.com>
2017-07-04 10:51:35 -0300

    adiciona exemplo

---
+++ exemplo.php
@@ -0,0 +1,12 @@
+<?php
+ require_once "vendor/autoload.php";
+
+ use \Petry\Translate\Locale\pt_BR;
+ use \Petry\Translate\Locale\en_US;
+
+$txtSource = "Eu te amo";
+$txtExpected = "I love you";
+
+$translate = new \Petry\Translate\Translate($txtSource, new pt_BR, new en_US);
+
+var_dump($translate->getTranslate());
